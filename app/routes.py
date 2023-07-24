630b612123f93e776c90eaeb80b3a1e265f90dbb
jagan010101 <jaganathapandiyan12@gmail.com>
2023-07-24 13:04:52 +0530

    small changes

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template
-from app import app, db
+from app import app
 from app.models import Yesterday
 
 import yfinance as yf
@@ -22,32 +22,36 @@ def update():
 @app.route('/home', methods=['GET', 'POST'])
 def home():
 
-    df = yf.download(tickers = ["ACCELYA.NS", "AFFLE.NS", "DEEPAKFERT.NS", "FEDERALBNK.NS", "UJJIVANSFB.NS", "ITC.NS", "TATASTEEL.NS", "HINDALCO.NS", "MPHASIS.NS", "AMARAJABAT.NS"], period="1d")['Adj Close']
-    df.columns = ["Accelya Solutions", "Affle", "Amara Raja Batteries", "Deepak Fertilizers", "Federal Bank", "Hindalco", "ITC", "Mphasis", "Tata Steel", "Ujjivan"]
+    df = yf.download(tickers = ["ACCELYA.NS", "AFFLE.NS", "DEEPAKFERT.NS", "FEDERALBNK.NS", "UJJIVANSFB.NS", "ITC.NS", "TATASTEEL.NS", "HINDALCO.NS", "MPHASIS.NS", "SBIN.NS", "AMARAJABAT.NS"], period="1d")['Adj Close']
+    df.columns = ["Accelya Solutions", "Affle", "Amara Raja Batteries", "Deepak Fertilizers", "Federal Bank", "Hindalco", "ITC", "Mphasis", "State Bank of India", "Tata Steel", "Ujjivan"]
     df = df.transpose()
     df.reset_index(inplace = True)
     df.columns = ['Company', 'Price']
 
-    mf = yf.download(tickers = ['0P0000K1D7.BO', '0P0000U9KG.BO'], period="1mo")['Adj Close'].iloc[-1]
-    mf.index = ['Aditya Birla Sun Life Medium Term (G)', 'Axis Gold Fund (G)']
+    mf1 = yf.download(tickers = '0P0000K1D7.BO', period="1mo")['Adj Close'].iloc[-1]
+    mf2 = yf.download(tickers = '0P0000U9KG.BO', period="1mo")['Adj Close'].iloc[-1] 
 
-    df1 = pd.DataFrame({"Company": mf.index, "Price": mf.values})
+    df.loc[len(df)] = ['Aditya Birla Sun Life Medium Term (G)', mf1]
+    df.loc[len(df)] = ['Axis Gold (G)', mf2]
+    df['Invested'] = [1378.39, 1975.41, 3100.65, 5581.1, 2533.48, 4709.48, 2262.48, 4402.58, 2857.5, 2064.92, 3046.28, 5999.70, 4999.75]
+    df['Units'] = [1, 2, 5, 10, 20, 11, 5, 2, 5, 19, 81, 183.8, 277.105]
+    df['Value'] = df['Price'] * df['Units']
+    df['Returns'] = df["Value"] - df["Invested"]
+    df_sum = pd.DataFrame({'Company': ['Total'], 'Value': [df['Value'].sum()], 'Invested': [df['Invested'].sum()], 'Returns': [df['Value'].sum() - df['Invested'].sum()]})
+    df = pd.concat([df, df_sum], ignore_index = True)
 
-    df2 = pd.concat([df,df1], ignore_index = True)
-    df2['Invested'] = [1378.39, 1975.41, 3100.65, 5581.1, 2533.48, 4709.48, 2262.48, 8094.28, 2064.92, 3046.28, 5999.70, 4999.75]
-    df2['Units'] = [1, 2, 5, 10, 20, 11, 5, 4, 19, 81, 183.8, 277.105]
-    df2['Value'] = df2['Price'] * df2['Units']
-    df2['Returns'] = df2["Value"] - df2["Invested"]
-    df_sum = pd.DataFrame({'Company': ['Total'], 'Value': [df2['Value'].sum()], 'Invested': [df2['Invested'].sum()], 'Returns': [df2['Value'].sum() - df2['Invested'].sum()]})
-    df2 = pd.concat([df2, df_sum], ignore_index = True)
+    df_db = pd.DataFrame({'Company': [0] * len(df), 'Yesterday': [0] * len(df)})
 
     for index, stock in enumerate(Yesterday.query.all()):
-        df2.loc[index, 'Yesterday'] = stock.yesterday
+        df_db.loc[index, 'Yesterday'] = stock.yesterday
+        df_db.loc[index, 'Company'] = stock.name
 
-    df2['Gain Today'] = df2['Value'] - df2['Yesterday']
-    df2['Gain Today %'] = 100 * df2['Gain Today']/ df2['Invested']
+    df = pd.merge(df, df_db, how='inner')
+    
+    df['Gain Today'] = df['Value'] - df['Yesterday']
+    df['Gain Today %'] = 100 * df['Gain Today']/ df['Invested']
 
     global df3
-    df3 = df2
+    df3 = df
 
-    return render_template('home.html', title = 'Stocks', df = df2)
+    return render_template('home.html', title = 'Stocks', df = df)
